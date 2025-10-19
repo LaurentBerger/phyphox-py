d1973f87d0676425aa8087996f1f7ea5816c0abb
unknown <3591626+LaurentBerger@users.noreply.github.com>
2025-10-19 18:13:32 +0200

    publish

--- src/phyphox/phyphox.py
+++ src/phyphox/phyphox.py
@@ -482,31 +482,37 @@ if __name__ == '__main__':
     else:
         logging.basicConfig(format="%(levelname)s: %(message)s")
     print("POLLING DATA")
-    my_phone = PhyphoxLogger("192.168.0.4", 8080, no_proxy=True)
-    # my_phone = PhyphoxLogger("192.168.0.2", 9899, no_proxy=True)
-    my_phone.meta_cmd()
-    my_phone.config_cmd()
-    print(my_phone)
-    my_phone.print_buffer_name()
-    if my_phone.buffer_needed([(0, 0, 1, 2, 3, 4)]):
-        my_phone.clear_cmd()
-        my_phone.print_select_buffer()
-        my_phone.start_cmd()
+    my_phones = [PhyphoxLogger("192.168.0.4", 8080, no_proxy=True),
+                 PhyphoxLogger("192.168.0.2", 9899, no_proxy=True)]
+                 
+    for my_phone in my_phones:
+        my_phone.meta_cmd()
+        my_phone.config_cmd()
+        print(my_phone)
+        my_phone.print_buffer_name()
+    if my_phones[0].buffer_needed([(0, 0, 1, 2, 3, 4)]) and \
+       my_phones[1].buffer_needed([(0, 0, 1, 2, 3, 4)]):
+        for my_phone in my_phones:
+            my_phone.clear_cmd()
+            my_phone.print_select_buffer()
+            my_phone.clear_cmd()
+        for my_phone in my_phones:
+            my_phone.start_cmd()
         time.sleep(1)
         for _ in range(10):
-            my_phone.get_measure(full_data=False)
-            print(my_phone.get_nb_measure())
-            last_tab = my_phone.get_last_measure()
-            if my_phone.new_data:
-                if my_phone.overflow:
-                    print("PhyphoxLogger overflow")
-                for tabs in last_tab:
-                    for tab in tabs:
-                        print(len(tab), len(tab))
-                my_phone.new_data = False
+            for my_phone in my_phones:
+                my_phone.get_measure(full_data=True)
+                print(my_phone.get_nb_measure())
+                last_tab = my_phone.get_last_measure()
+                if my_phone.new_data:
+                    if my_phone.overflow:
+                        print("PhyphoxLogger overflow")
+                    for tabs in last_tab:
+                        for tab in tabs:
+                            print(len(tab), len(tab))
+                    my_phone.new_data = False
             time.sleep(1)
-        my_phone.stop_cmd()
+        for my_phone in my_phones:
+            my_phone.stop_cmd()
     else:
         print("Invalid buffer selected")
-    my_phone.meta_cmd()
-    my_phone.config_cmd()
