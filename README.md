a34bba59176f719c5e6b8f302cb7d64b68c9c8c4
LaurentBerger <laurent.berger@univ-lemans.fr>
2025-10-20 14:25:35 +0200

    doc sample

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-phyphox-py
-Python interface to phyphox web interface 
+# phyphox-py
+Use phyphox-py package  to get experimental data from your phyphox phone app. 
+
+The purpose of this library is to get sensor data  from the phyphox app (see www.phyphox.org) in python.
+
+## Concept of phyphox
+
+Phyphox is an open source app that has been developed at the RWTH Aachen University. It is available on Android and iOS and primarily aims at making the phone's sensors available for physics experiments. 
+
+## Installation
+
+To install phyphox-py run this command in your terminal :
+
+```python3 -m pip install phyphox-py```
+
+or in Windows
+
+```python -m pip install phyphox-py```
+
+## Usage
+
+To use phyphox-py from command line:
+```
+import phyphox
+
+my_phone = phyphox.PhyphoxLogger("192.168.0.4", 8080)
+
+```
+
+
+## Credits
+
+This library has been developed by Laurent Berger with the help of phyphox forum and github issue.
+
+## Licence
+
+This library is released under the GNU Lesser General Public Licence v3.0.
+
+## Contact
+
+Contact me any time create an issue.
+
---
+++ samples/sample1.py
@@ -0,0 +1,48 @@
+import time
+import argparse
+import phyphox
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog='phyphox.py',
+        description='Python interface to phyphox REST API',
+        epilog='Text at the bottom of help')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true')
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(format="%(levelname)s: %(message)s",
+                            level=logging.DEBUG)
+        logging.info("Verbose output.")
+    else:
+        logging.basicConfig(format="%(levelname)s: %(message)s")
+    print("POLLING DATA")
+    my_phones = PhyphoxLogger("192.168.0.4", 8080, no_proxy=True)
+                 
+    my_phone.meta_cmd()
+    my_phone.config_cmd()
+    print(my_phone)
+    my_phone.print_buffer_name()
+    if my_phones[0].buffer_needed([(0, 0, 1, 2, 3, 4)]):
+        my_phone.clear_cmd()
+        my_phone.print_select_buffer()
+        my_phone.clear_cmd()
+        my_phone.start_cmd()
+        time.sleep(1)
+        for _ in range(10):
+            my_phone.get_measure(full_data=True)
+            print(my_phone.get_nb_measure())
+            last_tab = my_phone.get_last_measure()
+            if my_phone.new_data:
+                if my_phone.overflow:
+                    print("PhyphoxLogger overflow")
+                for tabs in last_tab:
+                    for tab in tabs:
+                        print(len(tab), len(tab))
+                my_phone.new_data = False
+            time.sleep(1)
+        my_phone.stop_cmd()
+    else:
+        print("Invalid buffer selected")
